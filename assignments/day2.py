606e26db75fb7f512ce9d0e5328bc17f49ea5fc3
muralidharan682 <m.rajendran@wolterskluwer.com>
2021-04-22 15:12:19 +0530

    Day2 Assignment

---
+++ assignments/day2.py
@@ -0,0 +1,12 @@
+skill_set = "python"
+hands_on = True
+experience = 1.5
+
+print("My skill set is {}".format(skill_set))
+print("{}, i have hands-on with python".format(hands_on))
+print("I have been working with python for {} years".format(experience))
+
+input1 = "10"
+input2 = "5"
+output = int(input1) + int(input2)
+print("{} + {} is {}".format(input1, input2, output))
